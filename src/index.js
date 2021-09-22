7d9673dbe5b0f58d094c29d337de8ec386988d2a
TanishqSingla <tanishqsingla00@gmail.com>
2021-09-23 01:51:01 +0530

    resolving error

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 const { ApolloServer } = require("apollo-server");
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
 
 // dummy data
-const links = [{
+const links = [
+  {
     id: "link-0",
     description: "This is the first feed on codernews",
-    url: "localhost:4000/post"
-}]
+    url: "localhost:4000/post",
+  },
+];
 
 const resolvers = {
-    Query: {
-        info: () => `This is the api of codernews`,
-        feed: () => links
-    },
-    Mutation: {
-        post: (parent, args) => {
-            let idCount = links.length
+  Query: {
+    info: () => `This is the api of codernews`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
 
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(links)
-            return link
-        }
-    }
-}
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
-    resolvers
-})
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.gql"), "utf8"),
+  resolvers,
+});
 
-server.listen().then(({ url }) => console.log(`Server is running at ${url}`))
+server.listen().then(({ url }) => console.log(`Server is running at ${url}`));
