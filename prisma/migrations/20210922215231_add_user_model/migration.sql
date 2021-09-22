aae47d3f850fc14012210c007ef708dd3f5e1c76
TanishqSingla <tanishqsingla00@gmail.com>
2021-09-23 03:27:24 +0530

    Adding mutation types for new schema

--- prisma/migrations/20210922215231_add_user_model/migration.sql
+++ prisma/migrations/20210922215231_add_user_model/migration.sql
@@ -1,11 +1,9 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-);
+/*
+  Warnings:
 
+  - You are about to drop the column `userId` on the `Link` table. All the data in the column will be lost.
+
+*/
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_Link" (
@@ -13,14 +11,11 @@ CREATE TABLE "new_Link" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "description" TEXT NOT NULL,
     "url" TEXT NOT NULL,
-    "userId" INTEGER,
-    CONSTRAINT "Link_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+    "postedById" INTEGER,
+    CONSTRAINT "Link_postedById_fkey" FOREIGN KEY ("postedById") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 INSERT INTO "new_Link" ("createdAt", "description", "id", "url") SELECT "createdAt", "description", "id", "url" FROM "Link";
 DROP TABLE "Link";
 ALTER TABLE "new_Link" RENAME TO "Link";
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
